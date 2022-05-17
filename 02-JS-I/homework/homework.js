f6e6e54269656263d0a2319723def47deb2e8f63
Leandro Testa <leandrotesta76@outlook.es>
2022-05-17 19:25:33 -0300

    homework VI completed

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -188,35 +188,38 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return nombre + apellido;
+  return nombre + ' '+ apellido;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var saludo = 'Hello ' + nombre + '!';
+  var saludo = 'Hello ' +' '+ nombre + '!';
   return saludo; 
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+      var area = alto*ancho;
+      return area;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+    var perimetro = lado*4;
+    return perimetro;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+        var artri = (base*altura)/2;
+        return artri;
 }
 
 
@@ -236,7 +239,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+      if(letra.legth>1){
+        return 'false date'
+      }
+      if(letra === 'a' ||letra === 'e' ||letra === 'i' ||letra === 'o' ||letra === 'u'){
+        return 'It s a vocal'
+      }else{
+        return 'itsnt a vocal'
+      }
 }
 }
 
